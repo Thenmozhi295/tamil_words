2eeb250ff6699f3476eec502f5b832d1117b542e
Thenmozhi295 <thenuselva1999@gmail.com>
2021-08-03 11:55:02 +0530

    tamil words for the site

---
+++ amuttu _program/amuttu_frequency_words.py
@@ -0,0 +1,12 @@
+#frequency of the each word
+
+from collections import Counter
+def word_count(fname):
+        with open(fname) as f:
+                return Counter(f.read().split())
+
+print("Number of words in the file :",word_count("/home/thenmozhi/amuttu_unigram.txt"))
+
+
+
+
---
+++ amuttu _program/amuttu_site.py
@@ -0,0 +1,33 @@
+
+import re
+from bs4 import BeautifulSoup
+from nltk.util import ngrams
+import tamil
+from tamil.utf8 import get_words
+
+htmlfile =open("/home/thenmozhi/amuttu.net/all.html","r")
+index=htmlfile.read()
+
+s=BeautifulSoup(index,'html.parser')
+#print(s.prettify())
+
+text=s.get_text(strip=True)
+#print(text)
+
+text3=re.sub(r"\d","",text)
+
+text4=re.sub(r"[a-zA-z]","",text3)
+
+text5=re.sub(r"[•©«()..?,–]","",text4)
+
+text6=re.sub(r"^\s+","",text5)
+#print(text6)
+
+#to print individual words
+
+word=get_words(text6)
+
+with open("/home/thenmozhi/amuttu.net/amuttu_unigram.txt","w") as f: 
+  for words in word:
+     print(words)
+     f.writelines("%s\n"%words)
